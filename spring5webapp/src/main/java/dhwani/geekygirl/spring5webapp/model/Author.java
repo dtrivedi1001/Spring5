a1c7aa4f6a88cd866e91ccc9dd96cf165da3531e
dhwanitrivedi <dhwani.trivedi1986@gmail.com>
2019-02-06 15:40:54 -0500

    JPA annotation added with getters and setters

--- spring5webapp/src/main/java/dhwani/geekygirl/spring5webapp/model/Author.java
+++ spring5webapp/src/main/java/dhwani/geekygirl/spring5webapp/model/Author.java
@@ -2,6 +2,7 @@ package dhwani.geekygirl.spring5webapp.model;
 
 import javax.persistence.*;
 import java.util.HashSet;
+import java.util.Objects;
 import java.util.Set;
 
 @Entity
@@ -13,7 +14,7 @@ public class Author {
     private String firstName;
     private String lastName;
 
-    @ManyToMany
+    @ManyToMany(mappedBy = "authors")
     private Set<Book> books = new HashSet<>();
 
     public Author(){
@@ -56,5 +57,28 @@ public class Author {
 
     public void setBooks(Set<Book> books) {
         this.books = books;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Author author = (Author) o;
+        return id == author.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Author{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", books=" + books +
+                '}';
     }
 }
